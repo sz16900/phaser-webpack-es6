3cae50aeaa1fed3da11230ad60303cedb798dacd
sz16900 <sz16900@my.bristol.ac.uk>
2020-07-22 13:52:48 -0500

    add optimization to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 const path = require('path');
 const copyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
-
 module.exports = {
   entry: {
     app: './src/index.js',
+    'production-dependencies': ['phaser'],
   },
+
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'build'),
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: 'production-dependencies',
+          chunks: 'initial',
+          minChunks: 2,
+        },
+      },
+    },
   },
 
   plugins: [
@@ -18,14 +31,18 @@ module.exports = {
           from: path.resolve(__dirname, 'index.html'),
           to: path.resolve(__dirname, 'build'),
         },
+        {
+          from: path.resolve(__dirname, 'assets', '**', '*'),
+          to: path.resolve(__dirname, 'build'),
+        },
       ],
     }),
+
     new webpack.DefinePlugin({
       'typeof CANVAS_RENDERER': JSON.stringify(true),
       'typeof WEBGL_RENDERER': JSON.stringify(true),
     }),
   ],
-
   module: {
     rules: [
       {
